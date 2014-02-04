0a349dda732a6341686a2ae75d8426873c967f43
Rajivhost <rajiv.mounguengue@hotmail.fr>
2014-02-04 13:05:01 +0100

    (+) Included preloading assemblies on bootstrapper sequence.

--- src/Catel.LogAnalyzer/Bootstrapper.cs
+++ src/Catel.LogAnalyzer/Bootstrapper.cs
@@ -1,16 +1,22 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Bootstrapper.cs" company="Catel development team">
-//   Copyright (c) 2008 - 2013 Catel development team. All rights reserved.
+//   Copyright (c) 2008 - 2014 Catel development team. All rights reserved.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace Catel.LogAnalyzer
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Reactive.Concurrency;
     using IoC;
     using Logging;
     using MVVM;
+    using MVVM.Tasks;
     using MVVM.ViewModels;
+    using Reflection;
     using Views;
+    using Environment = Catel.Environment;
 
     /// <summary>
     /// The bootstrapper.
@@ -24,10 +30,10 @@ namespace Catel.LogAnalyzer
         public Bootstrapper()
         {
 #if DEBUG
-            LogManager.RegisterDebugListener();
+            LogManager.AddDebugListener();
 #endif
 
-            var serviceLocator = ServiceLocator.Default;
+            var serviceLocator = this.GetDependencyResolver().Resolve<IServiceLocator>();
             Environment.RegisterDefaultViewModelServices();
 
             var viewLocator = serviceLocator.ResolveType<IViewLocator>();
@@ -46,8 +52,23 @@ namespace Catel.LogAnalyzer
         {
             base.ConfigureContainer();
 
+            Container.RegisterInstance<IScheduler>(TaskPoolScheduler.Default);
             Container.RegisterType<ILogAnalyzerService, LogAnalyzerService>();
             Container.RegisterType<IFileWatcherService, FileWatcherService>();
+        }
+
+        /// <summary>
+        ///     Initialize boot tasks.
+        /// </summary>
+        /// <param name="bootTasks">The additional boot tasks.</param>
+        /// <remarks>
+        /// Override this method to add additional tasks that will be executed before shell initialization.
+        /// </remarks>
+        protected override void InitializeBootTasks(IList<ITask> bootTasks)
+        {
+            var scheduler = TaskPoolScheduler.Default;
+
+            bootTasks.Add(new ActionTask("Preloading assemblies", taskProgressTracker => scheduler.Schedule(() => AppDomain.CurrentDomain.PreloadAssemblies())));
         }
         #endregion
     }
